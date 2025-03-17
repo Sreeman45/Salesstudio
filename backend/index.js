7f95e94b0455889cd22697dd07d9d75bb09633c0
sreeman <sreemanlingala333@gmail.com>
2025-03-17 13:18:30 +0530

    m

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,7 @@ dotenv.config();
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-  origin:'https://salesstudio.vercel.app'
-}))
+app.use(cors())
 let usersIp = [];
 const coupons = [
   "ABCDEF5678",
